2579ce2c192d9b8a7ec4f9bb5eaa198e48fd4f24
silan <siland.dope@yahoo.fr>
2020-06-03 20:41:19 -0700

    make Heroku app function

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ require('dotenv').config();
 const app = require ('./lib/server.js');
 const port = process.env.PORT;
 const mongodb = process.env.MONGODB_URI;
-const singUpOrSignin = require('./lib/guessing-game.js');
+// const singUpOrSignin = require('./lib/guessing-game.js');
 
 app.start(port, mongodb);
-singUpOrSignin();
+// singUpOrSignin();
+
 
